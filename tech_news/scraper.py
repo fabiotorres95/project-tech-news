339ae16ebf243a266c0088bbe0709105e62d1a3f
fabiotorres95 <engfabiotorres@outlook.com>
2023-11-09 15:59:05 -0300

    created fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,10 +1,23 @@
 import requests
+import time
 
 
 # Requisito 1
 def fetch(url):
-    request = requests.get(url)
-    return request.text
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url,
+            timeout=3,
+            headers={"user-agent": "Fake user-agent"}
+        )
+
+        if response.status_code != 200:
+            return None
+        return response.text
+
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
