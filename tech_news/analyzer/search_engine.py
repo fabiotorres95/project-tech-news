321b07d21844393cc65b5221a0447eb5ffb560b5
fabiotorres95 <engfabiotorres@outlook.com>
2023-11-14 15:07:31 -0300

    created search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,18 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    if not isinstance(title, str):
+        return []
+    title = title.lower()
+    query = {"title": {"$regex": title}}
+    data = search_news(query)
+    result = []
+    for news in data:
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 8
